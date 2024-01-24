f079fa462cc358ae98fec013380d7e46301ce394
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-01-24 01:51:13 +0100

    upd:v added pallindromic subsrtrings code

--- src/main/java/io/github/chhabra_dhiraj/Strings.java
+++ src/main/java/io/github/chhabra_dhiraj/Strings.java
@@ -5,6 +5,7 @@ public class Strings {
     public static void main(String[] args) {
         System.out.println(isPalindrome("Hello"));
         System.out.println(isPalindrome("abccba"));
+        printPalindromicSubstrings("abccba");
     }
 
     private static boolean isPalindrome(String str) {
@@ -23,5 +24,23 @@ public class Strings {
             j--;
         }
         return true;
+    }
+
+    private static void printPalindromicSubstrings(String str) {
+        if (str == null || str.length() == 0) {
+            System.out.println("Null or Empty String");
+            return;
+        }
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                String ss = str.substring(i, j);
+                if (isPalindrome(ss)) {
+                    System.out.println(ss);
+                }
+            }
+        }
     }
 }
