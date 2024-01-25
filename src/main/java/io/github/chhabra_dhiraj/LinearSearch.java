5fe3e216c13c742a501470a2e5d647722e4a971f
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-01-25 20:51:36 +0100

    upd: Linear Search: find Even Digits Num Brute Force

--- src/main/java/io/github/chhabra_dhiraj/LinearSearch.java
+++ src/main/java/io/github/chhabra_dhiraj/LinearSearch.java
@@ -3,8 +3,11 @@ package io.github.chhabra_dhiraj;
 public class LinearSearch {
 
     public static void main(String[] args) {
-        int[] arr = {4, 6, 7, 2, 8};
+        int[] arr = {4, 6, 7, 2, 8, 222, 22, 0, 11, 01};
         System.out.println(findMin(arr));
+        System.out.println("============================");
+        System.out.println("============================");
+        System.out.println(findEvenDigitNum(arr));
     }
 
     private static int findMin(int[] arr) {
@@ -12,13 +15,44 @@ public class LinearSearch {
             return -1;
         }
         int min = arr[0];
-        for(int i = 1; i < arr.length; i++) {
+        for (int i = 1; i < arr.length; i++) {
             int next = arr[i];
-            if(min > arr[i]) {
+            if (min > arr[i]) {
                 min = next;
             }
         }
 
         return min;
+    }
+
+    private static int findEvenDigitNum(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+
+        int count = 0;
+
+        for (int num : arr) {
+            if (isNumDigitsEven(num)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static boolean isNumDigitsEven(int num) {
+        if (num == 0) {
+            return false;
+        }
+
+        int count = 0;
+
+        while (num != 0) {
+            num = num / 10;
+            count++;
+        }
+
+        return count % 2 == 0;
     }
 }
