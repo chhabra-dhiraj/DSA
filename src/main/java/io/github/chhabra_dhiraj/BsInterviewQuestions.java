8ea9b327ef3b7b4533a3c7dfd883c87c0be73057
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-01-26 14:03:35 +0100

    upd: Binary Search: smallest letter

--- src/main/java/io/github/chhabra_dhiraj/BsInterviewQuestions.java
+++ src/main/java/io/github/chhabra_dhiraj/BsInterviewQuestions.java
@@ -5,6 +5,10 @@ public class BsInterviewQuestions {
     public static void main(String[] args) {
         int[] arr = {1, 2, 5, 7, 9, 21};
         System.out.println(getCeilingNumber(arr, 20));
+        System.out.println("=================================");
+        System.out.println("=================================");
+        char[] charArr = {'a', 'b', 'd', 'j'};
+        System.out.println(getSmallestLetter(charArr, 'c'));
     }
 
     // Ceiling of a target number
@@ -30,5 +34,32 @@ public class BsInterviewQuestions {
         }
 
         return start;
+    }
+
+    // Smallest letter greater than target
+    private static char getSmallestLetter(char[] letters, char target) {
+        if (letters == null || letters.length == 0) {
+            return '\u0000';
+        }
+
+        if(target >= letters[letters.length - 1]) {
+            return letters[0];
+        }
+
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int midElement = letters[mid];
+
+            if(target >= midElement) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return letters[start];
     }
 }
