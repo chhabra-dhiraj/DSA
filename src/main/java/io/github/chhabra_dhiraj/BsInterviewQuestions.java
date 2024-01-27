7ed14d1f060d86c809754d9a2546f5d5393a48b0
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-01-27 12:19:37 +0100

    upd: inflection number brute force

--- src/main/java/io/github/chhabra_dhiraj/BsInterviewQuestions.java
+++ src/main/java/io/github/chhabra_dhiraj/BsInterviewQuestions.java
@@ -9,6 +9,10 @@ public class BsInterviewQuestions {
         System.out.println("=================================");
         char[] charArr = {'a', 'b', 'd', 'j'};
         System.out.println(getSmallestLetter(charArr, 'c'));
+        System.out.println("=================================");
+        System.out.println("=================================");
+        int[] inflectionArray = {3, 1, 2, 7, 9, 8};
+        System.out.println(getInflectionNumber(inflectionArray));
     }
 
     // Ceiling of a target number
@@ -20,11 +24,11 @@ public class BsInterviewQuestions {
         int start = 0;
         int end = arr.length - 1;
 
-        while(start <= end) {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
             int midElement = arr[mid];
 
-            if(midElement == target) {
+            if (midElement == target) {
                 return mid;
             } else if (target > midElement) {
                 start = mid + 1;
@@ -42,7 +46,7 @@ public class BsInterviewQuestions {
             return '\u0000';
         }
 
-        if(target >= letters[letters.length - 1]) {
+        if (target >= letters[letters.length - 1]) {
             return letters[0];
         }
 
@@ -53,7 +57,7 @@ public class BsInterviewQuestions {
             int mid = start + (end - start) / 2;
             int midElement = letters[mid];
 
-            if(target >= midElement) {
+            if (target >= midElement) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
@@ -61,5 +65,46 @@ public class BsInterviewQuestions {
         }
 
         return letters[start];
+    }
+
+    private static int getInflectionNumber(int[] arr) {
+        if (arr == null || arr.length < 3) {
+            return -1;
+        }
+        for (int i = 1; i <= arr.length - 2; i++) {
+            if (checkLeftSide(arr, i) && checkRightSide(arr, i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private static boolean checkLeftSide(int[] arr, int index) {
+        int checkElement = arr[index];
+        int start = 0;
+        int end = index - 1;
+
+        for (int i = end; i >= start; i--) {
+            if (checkElement < arr[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean checkRightSide(int[] arr, int index) {
+        int checkElement = arr[index];
+        int start = index + 1;
+        int end = arr.length - 1;
+
+        for (int i = end; i >= start; i--) {
+            if (checkElement > arr[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
