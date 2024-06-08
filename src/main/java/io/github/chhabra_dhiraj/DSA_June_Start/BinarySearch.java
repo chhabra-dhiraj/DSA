a1355df526e327f361023ca5e84ad8df11dfe06e
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-06-08 17:59:03 +0530

    upd: binary search in perfect matrix

--- src/main/java/io/github/chhabra_dhiraj/DSA_June_Start/BinarySearch.java
+++ src/main/java/io/github/chhabra_dhiraj/DSA_June_Start/BinarySearch.java
@@ -1,11 +1,19 @@
 package io.github.chhabra_dhiraj.DSA_June_Start;
 
+import java.util.Arrays;
+
 public class BinarySearch {
 
     public static void main(String[] args) {
         int[] arr = {24, 9, 8, -2, -4};
 
         System.out.println(binarySearch(arr, 27));
+
+        System.out.println("=================================");
+        System.out.println("=================================");
+
+        int[][] matrix = {{10, 20, 30, 40}, {15, 25, 35, 45}, {28, 29, 37, 49}, {33, 34, 38, 50}};
+        System.out.println(Arrays.toString(binarySearchMatrix(matrix, 10)));
     }
 
     private static int binarySearch(int[] arr, int element) {
@@ -40,5 +48,22 @@ public class BinarySearch {
         }
 
         return -1;
+    }
+
+    private static int[] binarySearchMatrix(int[][] matrix, int target) {
+        int row = 0, column = matrix.length - 1;
+        while (row < matrix.length && column >= 0) {
+            if (target == matrix[row][column]) {
+                return new int[]{row, column};
+            }
+
+            if (target > matrix[row][column]) {
+                row++;
+            } else {
+                column--;
+            }
+        }
+
+        return new int[]{-1, -1};
     }
 }
