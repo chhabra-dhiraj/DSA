72dbdd9f04d3e853b7f9152c4f3f9cb96b675432
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-06-10 14:50:05 +0530

    upd: cyclic sort n consecutive numbers

---
+++ src/main/java/io/github/chhabra_dhiraj/DSA_June_Start/CyclicSort.java
@@ -0,0 +1,34 @@
+package io.github.chhabra_dhiraj.DSA_June_Start;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, 3, 1};
+
+        cyclicSortNConsec(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void cyclicSortNConsec(int[] arr) {
+        if (arr == null || arr.length == 0 || arr.length == 1) {
+            return;
+        }
+
+        int i = 0;
+        while (i < arr.length - 1) {
+            if (arr[i] != i + 1) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int s, int e) {
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
+    }
+}
