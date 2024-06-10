6ff3b9a3e2f4f53930239d6903b799c7b9603a33
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-06-10 16:50:16 +0530

    upd: isPallindrome String

---
+++ src/main/java/io/github/chhabra_dhiraj/DSA_June_Start/Strings.java
@@ -0,0 +1,29 @@
+package io.github.chhabra_dhiraj.DSA_June_Start;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String str = "aa";
+        System.out.println(isPalindrome(str));
+    }
+
+    private static boolean isPalindrome(String str) {
+        if(str == null || str.isBlank() || str.length() == 1) {
+            return true;
+        }
+
+        str = str.toLowerCase();
+
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
