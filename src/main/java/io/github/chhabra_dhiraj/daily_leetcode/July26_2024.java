aafe53b38183746d529413cd55f7020c74ceca85
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-07-26 14:36:20 +0530

    upd: basic structure = july26_2024 leetcode daily = 378. Kth Smallest Element in a Sorted Matrix

---
+++ src/main/java/io/github/chhabra_dhiraj/daily_leetcode/July26_2024.java
@@ -0,0 +1,20 @@
+package io.github.chhabra_dhiraj.daily_leetcode;
+
+public class July26_2024 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 5, 9},
+                {10, 11, 13},
+                {12, 13, 15}
+        };
+
+        int k = 8;
+
+        System.out.println(kthSmallest(matrix, k));
+    }
+
+    public static int kthSmallest(int[][] matrix, int k) {
+
+    }
+}
