be823ff03e9e53502aca8072badb73764454ba04
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-12-27 18:54:51 +0530

    upd: add 53. Maximum Subarray leetcode O(n) solution

---
+++ src/main/java/io/github/chhabra_dhiraj/daily_leetcode/Dec27_2024.java
@@ -0,0 +1,33 @@
+package io.github.chhabra_dhiraj.daily_leetcode;
+
+public class Dec27_2024 {
+
+    public static void main(String[] args) {
+        int[] nums = {-2, -5, -4, -3, -2, -1, -8};
+
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            sum += num;
+            if (sum > max) {
+                max = sum;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ src/main/java/io/github/chhabra_dhiraj/daily_leetcode/Nov14_2024.java
@@ -0,0 +1,14 @@
+package io.github.chhabra_dhiraj.daily_leetcode;
+
+public class Nov14_2024 {
+
+    public static void main(String[] args) {
+        int[] quantities = {11, 6};
+
+        int n = 6;
+    }
+
+//    public int minimizedMaximum(int n, int[] quantities) {
+//
+//    }
+}
